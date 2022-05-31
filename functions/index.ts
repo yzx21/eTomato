2e3202d8ec0589092ebe704519334ecac199f281
August Xie <yizhuoxie21@gmail.com>
2022-05-31 00:52:17 -0700

    First commit

---
+++ functions/index.ts
@@ -0,0 +1,6 @@
+import * as functions from "firebase-functions";
+import * as express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+exports.app = functions.https.onRequest(app);
